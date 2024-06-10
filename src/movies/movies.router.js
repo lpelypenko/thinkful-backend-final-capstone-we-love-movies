7ef38fcaa6ae2c077382ff3c8960036c799a938c
lpelypenko <2556875+lpelypenko@users.noreply.github.com>
2024-06-10 15:37:45 +0900

    cors enabled

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -1,6 +1,8 @@
 const router = require("express").Router({ mergeParams: true });
 const controller = require("./movies.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const cors = require("cors");
+router.use(cors());
 
 const reviewsRouter = require("../reviews/reviews.router");
 const theatersRouter = require("../theaters/theaters.router");
@@ -9,6 +11,6 @@ router.use("/:movieId/reviews", controller.movieExists, reviewsRouter);
 router.use("/:movieId/theaters", controller.movieExists, theatersRouter);
 
 router.route("/:movieId").get(controller.read).all(methodNotAllowed);
-router.route("/").get(controller.list).all(methodNotAllowed);
+router.route("/").all(cors()).get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
