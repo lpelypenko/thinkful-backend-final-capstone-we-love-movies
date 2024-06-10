fc3559d69f4b334bba2290c082297f7310a3d6e5
lpelypenko <2556875+lpelypenko@users.noreply.github.com>
2024-06-10 15:53:54 +0900

    fix issue with invalid path

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -7,10 +7,14 @@ router.use(cors());
 const reviewsRouter = require("../reviews/reviews.router");
 const theatersRouter = require("../theaters/theaters.router");
 
-router.use("/:movieId/reviews", controller.movieExists, reviewsRouter);
-router.use("/:movieId/theaters", controller.movieExists, theatersRouter);
+router.use("/:movieId([0-9]+)/reviews", controller.movieExists, reviewsRouter);
+router.use(
+  "/:movieId([0-9]+)/theaters",
+  controller.movieExists,
+  theatersRouter
+);
 
-router.route("/:movieId").get(controller.read).all(methodNotAllowed);
+router.route("/:movieId([0-9]+)").get(controller.read).all(methodNotAllowed);
 router.route("/").all(cors()).get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
